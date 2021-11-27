2b0857e13df1121ebe9b13e76ff35e455b2ef343
Julia_Nakano <88911932+JuliaNakano@users.noreply.github.com>
2021-11-26 21:45:24 -0300

    Solução do exercício "SequenciaCentena"

---
+++ Exercicio_SequenciaCentena/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercicio_SequenciaCentena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int n=0;
+            while(n<=100)
+            {
+            Console.Write($"{n} ");
+            n += 1;
+            }
+            
+
+        }
+    }
+}
